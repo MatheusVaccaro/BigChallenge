2b0d8c9e9a4f9361638085d541f3a394f6fce6da
Bruno Fulber Wide <bfwide07@gmail.com>
2018-06-21 17:14:51 -0300

    added date property and date extension

---
+++ BigChallenge/Helpers/dateExtension.swift
@@ -0,0 +1,15 @@
+//
+//  dateExtension.swift
+//  BigChallenge
+//
+//  Created by Bruno Fulber Wide on 21/06/18.
+//  Copyright © 2018 Matheus Vaccaro. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    
+    
+}
