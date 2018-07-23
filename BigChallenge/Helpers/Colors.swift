18577ebaf4d2fa519e15dce949a1f337d3071bf8
Bruno Fulber Wide <bfwide07@gmail.com>
2018-07-23 11:32:19 -0300

    added 3 more gradient colros

--- BigChallenge/Helpers/Colors.swift
+++ BigChallenge/Helpers/Colors.swift
@@ -17,5 +17,20 @@ struct Colors {
             UIColor(red: 115/255, green: 0, blue: 217/255, alpha: 1).cgColor
         ]
         
+        static let redGradient: [CGColor] = [
+            UIColor(red: 245/255, green: 81/255, blue: 95/255, alpha: 1).cgColor,
+            UIColor(red: 159/255, green: 4/255, blue: 27/255, alpha: 1).cgColor
+        ]
+        
+        static let peachGradient: [CGColor] = [
+            UIColor(red: 250/255, green: 97/255, blue: 190/255, alpha: 1).cgColor,
+            UIColor(red: 247/255, green: 107/255, blue: 28/255, alpha: 1).cgColor
+        ]
+        
+        static let greenGradient: [CGColor] = [
+            UIColor(red: 0, green: 238/255, blue: 151/255, alpha: 1).cgColor,
+            UIColor(red: 11/255, green: 179/255, blue: 83/255, alpha: 1).cgColor
+        ]
+        
     }
 }
