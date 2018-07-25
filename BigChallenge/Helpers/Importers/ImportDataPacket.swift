114b7ad63591f8cbc0b750993b212f32e59fa963
Max Zorzetti <maxzorzetti@live.com>
2018-07-24 21:11:50 -0300

    Add ImportDataPacket enum
    Contains import data for tasks that were imported from an external service.
    Only case so far is Reminders-related data.

---
+++ BigChallenge/Helpers/Importers/ImportDataPacket.swift
@@ -0,0 +1,23 @@
+//
+//  ImportInfoType.swift
+//  BigChallenge
+//
+//  Created by Max Zorzetti on 24/07/18.
+//  Copyright © 2018 Matheus Vaccaro. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Abstraction of data coming from external import sources.
+*/
+enum ImportDataPacket {
+    case remindersDataPacket(id: String, externalId: String?) // 1
+    
+    // Assigns an identifier to each import source. Required for CoreData
+    var type: Int16 {
+        switch self {
+        case .remindersDataPacket: return 1 	// Reminders
+        }
+    }
+}
