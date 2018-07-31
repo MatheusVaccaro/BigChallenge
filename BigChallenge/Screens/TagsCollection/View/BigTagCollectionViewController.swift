92f531ed553125d902d8d02f97f9eacb6ed354af
Bruno Fulber Wide <bfwide07@gmail.com>
2018-07-31 17:51:17 -0300

    fixed bigCollectionView unecessary override

--- BigChallenge/Screens/TagsCollection/View/BigTagCollectionViewController.swift
+++ BigChallenge/Screens/TagsCollection/View/BigTagCollectionViewController.swift
@@ -24,41 +24,9 @@ class BigTagCollectionViewController: TagCollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tagsLabel.text = Strings.Tag.CollectionScreen.title
-    }
-    
-    override func bindCollectionView() {
-        viewModel.tagsObservable
-            .map { return [Item(tag: nil)] + $0.map { Item(tag: $0) } }
-            .bind(to: tagsCollectionView.rx
-                .items(cellIdentifier: TagCollectionViewCell.identifier,
-                       cellType: TagCollectionViewCell.self)) { (row, item, cell) in
-                
-                        guard let tag = item.tag else {
-                            cell.configure()
-                            return
-                        }
-                        
-                        let viewModel = self.viewModel.tagCollectionCellViewModel(for: tag)
-                        let index = IndexPath(row: row, section: 0)
-                        cell.configure(with: viewModel)
-                        self.loadSelection(for: cell, tag: tag, at: index)
-                
-                        if row == self.viewModel.tags.count-1 {
-                            self.collectionViewHeightConstraint.constant =
-                                self.tagsCollectionView.collectionViewLayout.collectionViewContentSize.height
-                        }
-                
-            }.disposed(by: disposeBag)
         
-        tagsCollectionView.rx.modelSelected(Item.self).subscribe { event in
-            guard let tag = event.element?.tag else { return }
-            self.viewModel.selectedTagEvent.onNext(tag)
-        }.disposed(by: disposeBag)
-        
-        tagsCollectionView.rx.modelDeselected(Item.self).subscribe { event in
-            guard let tag = event.element?.tag else { return }
-            self.viewModel.selectedTagEvent.onNext(tag)
-        }.disposed(by: disposeBag)
+        self.collectionViewHeightConstraint.constant =
+            self.tagsCollectionView.collectionViewLayout.collectionViewContentSize.height
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
