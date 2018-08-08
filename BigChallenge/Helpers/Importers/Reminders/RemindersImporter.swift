1dceb375be0ddb676d0a0eb1c2843892a17ca51f
Matheus Vaccaro <matheusvaccaro@icloud.com>
2018-08-08 18:23:55 -0300

    Update convertTaskAndTag(from:) to user TaskModel's createTask(with:) correct interface

--- BigChallenge/Helpers/Importers/Reminders/RemindersImporter.swift
+++ BigChallenge/Helpers/Importers/Reminders/RemindersImporter.swift
@@ -108,16 +108,17 @@ public class RemindersImporter {
     
     // Convert a reminder to a Task and a Tag
     private func convertTaskAndTag(from reminder: Reminder) -> (task: Task, tag: Tag) {
-        let attributes: [TaskModel.Attributes : Any] =
-            [.title : reminder.title ?? ""]
-        let task = taskModel.createTask(with: attributes)
+        let taskAttributes: [TaskModel.Attributes : Any] =
+            [.title : reminder.title as Any,
+             .isCompleted : reminder.isCompleted as Any,
+             .completionDate : reminder.completionDate as Any,
+             .dueDate : reminder.completionDate as Any,
+             .creationDate : reminder.creationDate as Any
+             ]
+        let task = taskModel.createTask(with: taskAttributes)
+        
         let tag = tagModel.createTag(with: reminder.calendar.title)
     
-        task.isCompleted = reminder.isCompleted
-        task.completionDate = reminder.completionDate
-        task.dueDate = reminder.completionDate
-        task.creationDate = reminder.creationDate
-        
         task.addToTags(tag)
         
         taskModel.associate(task, with: reminder.dataPacket)
