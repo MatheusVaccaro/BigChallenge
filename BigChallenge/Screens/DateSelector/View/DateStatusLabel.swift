c77a1797d386e398242de0a7876811ab006a6935
Max Zorzetti <maxzorzetti@live.com>
2018-08-13 00:29:24 -0300

    Align shortcut buttons in DateInput screen to status text
    Also make a minor change in the status text for EN-US localization.

--- BigChallenge/Screens/DateSelector/View/DateStatusLabel.swift
+++ BigChallenge/Screens/DateSelector/View/DateStatusLabel.swift
@@ -13,8 +13,12 @@ class DateStatusLabel: UILabel {
     var colorLayer: CAShapeLayer?
     var shadowLayer: CAShapeLayer?
     var isToggled: Bool = false {
-        didSet { toggleBackground(to: isToggled) }
+        didSet { toggleBackground(on: isToggled) }
     }
+    
+    private static let toggledOnFont = UIFont.font(sized: 19.77, weight: .regular, with: .body)
+    private static let toggledOffFont = UIFont.font(sized: 19.77, weight: .semibold, with: .body)
+        
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,22 +26,12 @@ class DateStatusLabel: UILabel {
         self.isUserInteractionEnabled = true
         self.layer.masksToBounds = true
         self.layer.cornerRadius = 4.3
-        self.font = UIFont.font(sized: 19.77, weight: .semibold, with: .body)
+        self.font = DateStatusLabel.toggledOffFont
         self.textColor = UIColor.DateInput.defaultColor
-        
-//        configureColor()
-//        configureShadow()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-//        configureColor()
-//        configureShadow()
     }
 
     private func configureColor() {
@@ -72,16 +66,18 @@ class DateStatusLabel: UILabel {
         shadowLayer = newShadowLayer
     }
     
-    private func toggleBackground(to bool: Bool) {
+    private func toggleBackground(on bool: Bool) {
         if bool {
             colorLayer?.fillColor = UIColor.DateInput.defaultColor.cgColor
             backgroundColor = UIColor.DateInput.defaultColor
             textColor = .white
+            font = DateStatusLabel.toggledOnFont
             
         } else {
             colorLayer?.fillColor = UIColor.clear.cgColor
             backgroundColor = .clear
             textColor = UIColor.DateInput.defaultColor
+            font = DateStatusLabel.toggledOffFont
         }
     }
 }
