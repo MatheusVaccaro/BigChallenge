fc0598e7fbb6279d0140ce203c6270a590f509e0
Bruno Fulber Wide <bfwide07@gmail.com>
2018-08-14 23:36:23 -0300

    added show completed tasks event

--- BigChallenge/Screens/TaskList/ViewModel/TaskListViewModel.swift
+++ BigChallenge/Screens/TaskList/ViewModel/TaskListViewModel.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Crashlytics
 import UIKit
 import RxCocoa
 import RxSwift
@@ -21,6 +22,7 @@ public class TaskListViewModel {
     var showsCompletedTasks: Bool = false {
         didSet {
             print((showsCompletedTasks ? "showing" : "not showing") + " completed tasks")
+            if showsCompletedTasks { Answers.logCustomEvent(withName: "showed completed tasks") }
             tasksObservable.onNext((mainTasks, tasksToShow))
         }
     }
