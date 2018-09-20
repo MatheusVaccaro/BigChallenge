b896fbf200e7abe6fef3773fc7410dc726a2490f
Matheus Vaccaro <matheusvaccaro@icloud.com>
2018-09-20 20:04:16 -0300

    Add NotesInput storyboard and viewcontroller

---
+++ Reef/Screens/NotesInput/View/NotesInputViewController.swift
@@ -0,0 +1,45 @@
+//
+//  NotesInputViewController.swift
+//  Reef
+//
+//  Created by Matheus Vaccaro on 20/09/18.
+//  Copyright © 2018 Matheus Vaccaro. All rights reserved.
+//
+
+import UIKit
+import UITextView_Placeholder
+
+class NotesInputViewController: UIViewController {
+
+    // MARK: - Properties
+    var viewModel: NotesInputViewModel!
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var notesInputTextView: UITextView!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNotesInputTextView()
+    }
+    
+    // MARK: - Functions
+    private func configureNotesInputTextView() {
+        notesInputTextView.font = UIFont.font(sized: 22, weight: .semibold, with: .body, fontName: .barlow)
+        notesInputTextView.placeholder = viewModel.textViewPlaceholder
+        notesInputTextView.placeholderColor = UIColor.placeholder
+    }
+}
+
+// MARK: - StoryboardInstantiable
+extension NotesInputViewController: StoryboardInstantiable {
+    
+    static var viewControllerID: String {
+        return "NotesInputViewController"
+    }
+    
+    static var storyboardIdentifier: String {
+        return "NewTask"
+    }
+    
+}
