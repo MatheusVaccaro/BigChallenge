d561cc3d884c0e27d0aa54c4d36de2d49e945379
Max Zorzetti <maxzorzetti@live.com>
2018-09-21 17:42:18 -0300

    Change calendar date selection display to use a circle

--- Reef/Screens/DateInput/View/Calendar/CalendarCell.swift
+++ Reef/Screens/DateInput/View/Calendar/CalendarCell.swift
@@ -13,10 +13,29 @@ class CalendarCell: JTAppleCell {
     
     @IBOutlet weak var dayLabel: UILabel!
     @IBOutlet weak var roundedView: UIView!
+    private(set) var backgroundCircleLayer: CAShapeLayer!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        roundedView.layer.cornerRadius = roundedView.bounds.width/2
+        
+        backgroundCircleLayer = CAShapeLayer()
+        contentView.layer.addSublayer(backgroundCircleLayer)
+    }
+    
+    func setupBackgroundCircleLayer() {
+        let circleRadius = min(contentView.frame.width, contentView.frame.height) / 2
+        
+        let circleBounds =  CGRect(origin: CGPoint(x: max(0, contentView.frame.width - contentView.frame.height) / 2,
+                                                   y: max(0, contentView.frame.height - contentView.frame.width) / 2),
+                                   size: CGSize(width: circleRadius * 2, height: circleRadius * 2))
+        backgroundCircleLayer.path = UIBezierPath(roundedRect: circleBounds, cornerRadius: circleRadius).cgPath
+        backgroundCircleLayer.zPosition = -1000
+        backgroundCircleLayer.fillColor = UIColor.DateInput.Calendar.deselectedDateBackground.cgColor
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupBackgroundCircleLayer()
     }
     
     func configure(with cellState: CellState) {
@@ -31,13 +50,12 @@ class CalendarCell: JTAppleCell {
     }
     
     func select(basedOn cellState: CellState? = nil) {
-        dayLabel.textColor = UIColor.white
-        roundedView.backgroundColor = UIColor.black
+        dayLabel.textColor = UIColor.DateInput.Calendar.selectedDate
+        backgroundCircleLayer.fillColor = UIColor.DateInput.Calendar.selectedDateBackground.cgColor
     }
     
     func deselect(basedOn cellState: CellState? = nil) {
-        backgroundColor = UIColor.clear
-        roundedView.backgroundColor = UIColor.clear
+        backgroundCircleLayer.fillColor = UIColor.DateInput.Calendar.deselectedDateBackground.cgColor
         if let cellState = cellState, cellState.dateBelongsTo != .thisMonth {
             dayLabel.textColor = UIColor.DateInput.Calendar.dateOffCurrentMonth
         } else {
