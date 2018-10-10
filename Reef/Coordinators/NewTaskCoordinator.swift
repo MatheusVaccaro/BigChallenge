1a13a86c4a3212dd2113d011eac4fc88ac12cadd
Bruno Fulber Wide <bfwide07@gmail.com>
2018-10-10 12:55:49 -0300

    fixed tap on modalPresenter dismissing on detail screens

--- Reef/Coordinators/NewTaskCoordinator.swift
+++ Reef/Coordinators/NewTaskCoordinator.swift
@@ -123,13 +123,19 @@ class NewTaskCoordinator: NSObject, Coordinator {
         modalPresenter.navigationBar.isTranslucent = false
         modalPresenter.navigationBar.largeTitleTextAttributes = largeTitleAttributes
         modalPresenter.modalPresentationStyle = .overCurrentContext
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissViewController))
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleNavigationTap))
         modalPresenter.navigationBar.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc private func handleNavigationTap() {
+        guard modalPresenter.children.count == 1 else { return }
+        dismissViewController()
     }
 }
 
 extension NewTaskCoordinator {
-    @objc private func dismissViewController() {
+    private func dismissViewController() {
         presenter.dismiss(animated: true, completion: nil)
         delegate?.shouldDeinitCoordinator(self)
     }
