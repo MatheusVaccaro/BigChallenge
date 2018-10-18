6827f4ac8236bb27d2150b4187d36d62fc2a8718
Bruno Fulber Wide <bfwide07@gmail.com>
2018-10-18 14:36:43 -0300

    created extension

---
+++ ReefIntents/IntentHandler.swift
@@ -0,0 +1,20 @@
+//
+//  IntentHandler.swift
+//  ReefIntents
+//
+//  Created by Bruno Fulber Wide on 18/10/18.
+//  Copyright © 2018 Matheus Vaccaro. All rights reserved.
+//
+
+import Intents
+
+class IntentHandler: INExtension {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+}
