7ba4af677d390ad0d36c49b283168d5cf8031776
Matheus Vaccaro <matheusvaccaro@gmail.com>
2018-11-08 15:34:54 -0200

    Add localized random texts

--- Reef/Helpers/RandomSwipeTextGenerator.swift
+++ Reef/Helpers/RandomSwipeTextGenerator.swift
@@ -14,29 +14,42 @@ class RandomSwipeTextGenerator {
     
     let completed: [String]
     private var randomCompleted: [Int]!
+    private var lastCompletedIndex: Int = 0
     
     let readmitted: [String]
     private var randomReadmitted: [Int]!
+    private var lastReadmittedIndex: Int = 0
     
     let deleted: [String]
     private var randomDeleted: [Int]!
+    private var lastDeletedIndex: Int = 0
     
     private init() {
-        self.completed = [Strings.Task.Cell.Swipe.Text.completed]
-        self.readmitted = [Strings.Task.Cell.Swipe.Text.readmitted]
-        self.deleted = [Strings.Task.Cell.Swipe.Text.deleted]
+        self.completed = [Strings.Task.Cell.Swipe.Text.completed1,
+                          Strings.Task.Cell.Swipe.Text.completed2,
+                          Strings.Task.Cell.Swipe.Text.completed3,
+                          Strings.Task.Cell.Swipe.Text.completed4,
+                          Strings.Task.Cell.Swipe.Text.completed5]
+        self.readmitted = [Strings.Task.Cell.Swipe.Text.readmitted1,
+                           Strings.Task.Cell.Swipe.Text.readmitted2,
+                           Strings.Task.Cell.Swipe.Text.readmitted3,
+                           Strings.Task.Cell.Swipe.Text.readmitted4]
+        self.deleted = [Strings.Task.Cell.Swipe.Text.deleted1]
         
-        self.randomCompleted = generateRandomArray(size: completed.count)
-        self.randomReadmitted = generateRandomArray(size: readmitted.count)
-        self.randomDeleted = generateRandomArray(size: deleted.count)
+        self.randomCompleted = generateRandomArray(size: completed.count, notStartingWith: lastCompletedIndex)
+        self.randomReadmitted = generateRandomArray(size: readmitted.count, notStartingWith: lastReadmittedIndex)
+        self.randomDeleted = generateRandomArray(size: deleted.count, notStartingWith: lastDeletedIndex)
     }
     
-    private func generateRandomArray(size: Int) -> [Int] {
+    private func generateRandomArray(size: Int, notStartingWith value: Int) -> [Int] {
         var array: [Int] = []
-        for _ in 0..<size {
+        for index in 0..<size {
             var elementAdded = false
             repeat {
                 let random = Int(arc4random_uniform(UInt32(size)))
+                if size != 1 && index == 0 && random == value {
+                    continue
+                }
                 if !array.contains(random) {
                     array.append(random)
                     elementAdded = true
@@ -49,23 +62,32 @@ class RandomSwipeTextGenerator {
     func nextText(forTask taskType: RandomSwipeTextGenerator.TextType) -> String {
         switch taskType {
         case .completed:
-            guard let index = randomCompleted.first else {
-                randomCompleted = generateRandomArray(size: completed.count)
+            if randomCompleted.isEmpty {
+                randomCompleted = generateRandomArray(size: completed.count, notStartingWith: lastCompletedIndex)
                 return nextText(forTask: .completed)
+            } else {
+                let index = randomCompleted.removeFirst()
+                lastCompletedIndex = index
+                return completed[index]
             }
-            return completed[index]
         case .readmitted:
-            guard let index = randomReadmitted.first else {
-                randomReadmitted = generateRandomArray(size: readmitted.count)
+            if randomReadmitted.isEmpty {
+                randomReadmitted = generateRandomArray(size: readmitted.count, notStartingWith: lastReadmittedIndex)
                 return nextText(forTask: .readmitted)
+            } else {
+                let index = randomReadmitted.removeFirst()
+                lastReadmittedIndex = index
+                return readmitted[index]
             }
-            return readmitted[index]
         case .deleted:
-            guard let index = randomDeleted.first else {
-                randomDeleted = generateRandomArray(size: deleted.count)
+            if randomDeleted.isEmpty {
+                randomDeleted = generateRandomArray(size: deleted.count, notStartingWith: lastDeletedIndex)
                 return nextText(forTask: .deleted)
+            } else {
+                let index = randomDeleted.removeFirst()
+                lastDeletedIndex = index
+                return deleted[index]
             }
-            return deleted[index]
         }
     }
     
