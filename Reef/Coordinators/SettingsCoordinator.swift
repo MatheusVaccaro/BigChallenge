26603993f9eab2ef7c1015db4f89bc5fde7849c1
gpaulbr <gpaulbr@gmail.com>
2018-11-14 15:23:45 -0200

    adapt coordinator to show config screen

--- Reef/Coordinators/SettingsCoordinator.swift
+++ Reef/Coordinators/SettingsCoordinator.swift
@@ -20,12 +20,23 @@ class SettingsCoordinator: Coordinator {
     }
     
     func start() {
-        showThemeSelection()
+        showSettings()
     }
     
     private func showThemeSelection() {
         let settingsViewController = ThemeSelectViewController.instantiate()
         settingsViewController.title = Strings.Settings.Theme.title
+        
+        presenter.pushViewController(settingsViewController, animated: true)
+    }
+    
+    private func showSettings() {
+        let settingsViewController = SettingsListViewController.instantiate()
+        let settingsViewModel = SettingsListViewModel()
+        
+        settingsViewController.viewModel = settingsViewModel
+        
+        settingsViewController.title = "Settings" // TODO: Localize this
         
         presenter.pushViewController(settingsViewController, animated: true)
     }
